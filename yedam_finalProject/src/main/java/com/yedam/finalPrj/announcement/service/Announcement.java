9a0f8cabb5a58bffecc825f2e152b3035a0d8966
dbstjdghks2 <dbstjdghks21@naver.com>
2022-05-03 13:06:09 +0900

    db

--- yedam_finalProject/src/main/java/com/yedam/finalPrj/announcement/service/Announcement.java
+++ yedam_finalProject/src/main/java/com/yedam/finalPrj/announcement/service/Announcement.java
@@ -1,5 +1,18 @@
 package com.yedam.finalPrj.announcement.service;
 
-public class Announcement {
+import lombok.Getter;
+import lombok.Setter;
+import oracle.sql.DATE;
 
+@Setter
+@Getter
+public class Announcement {
+	private String announcementSerial;
+	private String announcementTitle;
+	private String announcementContent;
+	private DATE announcementDate;
+	private String announcementWriter;
+	private String announcementHidden;
+	private int announcementView;
+	
 }
