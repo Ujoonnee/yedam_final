e0fc37c1ae000c4e24be396377ca0566ad502b15
HongJe <dlghdwp2297@naver.com>
2022-05-04 17:55:50 +0900

    123

---
+++ yedam_finalProject/src/main/java/com/yedam/finalPrj/user/service/UserService.java
@@ -0,0 +1,13 @@
+package com.yedam.finalPrj.user.service;
+
+import java.util.List;
+
+public interface UserService {
+	UserVO findOne(UserVO vo);   // 단건 조회
+	UserVO findAll(UserVO vo);      // ID조회
+	int insert(UserVO vo);       // 등록
+	int update(UserVO vo);       // 수정
+	int delete(UserVO vo);       // 삭제
+	
+	boolean idCheck(String str); // id중복체크
+}
